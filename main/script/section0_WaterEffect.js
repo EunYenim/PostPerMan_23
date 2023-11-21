80c03cf8f603422f39b56541a5992ca054bb528b
EunYenim <126849556+EunYenim@users.noreply.github.com>
2023-11-21 14:01:57 +0900

    1

---
+++ main/script/section0_WaterEffect.js
@@ -0,0 +1,9 @@
+function setup() {
+  let canvas = createCanvas(windowWidth - 16.2, windowHeight);
+  canvas.parent('waterCanvas');
+  background('blue');
+}
+
+function draw() {
+  background('blue');
+}
